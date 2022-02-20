51804c1d15e73bd09808c507421679a5030acaad
Hollie Duncan <holduncan7@gmail.com>
2022-02-20 20:36:21 +0000

    added hamburger nav

--- public/js/script.js
+++ public/js/script.js
@@ -5,3 +5,13 @@ document.addEventListener(
   },
   false
 );
+
+// Nav Animation
+
+const menu_btn = document.querySelector('.hamburger');
+	const hamburger_menu = document.querySelector('.hamburger-nav');
+
+	menu_btn.addEventListener('click', function () {
+		menu_btn.classList.toggle('is-active');
+		hamburger_menu.classList.toggle('is-active');
+	});

0c88176aa475b971784c1f7ea611f22e89bd7594
Hollie Duncan <holduncan7@gmail.com>
2022-02-20 20:35:16 +0000

    Added hamburger nav

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -1,14 +1,20 @@
+// Imports
+
 const router = require("express").Router();
 const User = require("../models/User.model");
 const Client = require("../models/Client.model");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
+// API Info
+
 const axios = require('axios');
 
 const api = axios.create({
     baseURL: 'https://www.thecolorapi.com'
   })
+
+  // Create Client Page
 
 router.get("/:id/client-page", isLoggedIn, (req, res, next) =>{
     const {id} = req.params;
@@ -23,13 +29,40 @@ module.exports = router;
 
 // // Add New Color 
 
-router.get("/:id/client-color", isLoggedIn, (req, res, next) => {
+router.get("/:id/client-color", isLoggedIn, async (req, res, next) => {
 
-    res.render("client/client-color")
+    try {
+
+
+    const {id} = req.params;
+    let colorInfo = []
+
+    const foundClient = await Client.findById(id);
+
+    console.log(foundClient)
+
+    Promise.all(foundClient.colorPalette.map(async(color) => {
+
+      let colorData = await axios.get(`https://www.thecolorapi.com/id?hex=${color}`)
+        colorInfo.push(colorData.data)
+        console.log('inside map', colorInfo)
+
+    }))
+
+        console.log("color infooooo", colorInfo)
+        res.render("client/client-color")
+
+}
+
+catch(err) {
+
+console.log(err)
+
+}
 
 })
 
-
+    // res.render("client/client-color", {client: foundClient, colorInfo: apiData.data})
 
 router.post("/:id/new-color", (req, res, next) => {
 
@@ -42,7 +75,21 @@ router.post("/:id/new-color", (req, res, next) => {
         console.log(updatedClient)
     
         res.redirect(`/client/${id}/client-page`)
+
     })
+
     .catch((err) => next((err)))
 });
+
+// Get color info from API
+
+
+
+    // axios.get(`https://www.thecolorapi.com/id?hex=${color}`)
+    // .then((colorData)=> {
+
+
+
+    // })
+
 

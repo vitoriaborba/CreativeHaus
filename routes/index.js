6c46e203dec31143c1cfacfb4263d0797158a6b6
Vitoria Borba <vitoriaborba@icloud.com>
2022-02-21 18:33:41 +0000

    Basic functionalities complete

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const axios = require('axios');
-
-
+const async = require("hbs/lib/async");
+const api = axios.create({
+  baseURL: 'https://www.thecolorapi.com'
+})
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
@@ -14,11 +16,24 @@ router.get("/color-palette", (req, res, next) => {
 res.render("color-palette");
 })
 
-router.post("/color-palette-select", (req, res, next) => {
-  const colorhex = req.body;
-  const colorScheme = axios.get(`https://www.thecolorapi.com/scheme?hex=${colorhex}&mode=triad&count=5`)
-  .then(() =>{
-    res.render("color-palette", {colorScheme})
-  })
+router.post("/color-palette-select", async (req, res, next) => {
+  const colorhex = req.body.colorhex.split('#')[1]
+  const paletteOption = req.body.paletteOption
+  let colorScheme;
+  if (!paletteOption) {
+    return res
+      .status(400)
+      .render("color-palette", { errorMessage: "Please select a palette option." });
+  }
+  try {
+    colorScheme = await axios.get(`https://www.thecolorapi.com/scheme?hex=${colorhex}&mode=${paletteOption}&count=5`)
+    console.log(colorhex)
+    console.log(colorScheme.colours)
+
+    res.render("color-palette", {colorScheme: colorScheme.data})
+  }
+  catch(err) {
+    next(err);
+  }
 })
 

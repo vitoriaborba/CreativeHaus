1dcd6db5ba454a991baac6a560ee5d4c061a9bef
Vitoria Borba <vitoriaborba@icloud.com>
2022-02-24 10:58:24 +0000

    switch buttom working

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,15 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
+    
     console.log("project2 JS imported successfully!");
+    if ( localStorage.getItem('theme') === 'dark'){
+      document.getElementById("body").classList.remove("light-mode");
+      checkbox.checked = false;
+    } else {
+      checkbox.checked = true;
+      document.getElementById("body").classList.add("light-mode");
+    }
   },
   false
 );
@@ -15,3 +23,17 @@ const menu_btn = document.querySelector('.hamburger');
 		menu_btn.classList.toggle('is-active');
 		hamburger_menu.classList.toggle('is-active');
 	});
+
+  document.cookie = "light-mode";
+
+  if(document.cookie){
+  function handleChange(checkbox) {
+    if(checkbox.checked == true){
+         document.getElementById("body").classList.add("light-mode");
+        localStorage.setItem("theme", "light");
+    }else{
+        document.getElementById("body").classList.remove("light-mode");
+        localStorage.setItem("theme", "dark");
+   }
+ } 
+}
